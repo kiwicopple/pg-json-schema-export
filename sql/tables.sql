3e1fbce1e7d344806544faecdf59babc24071516
Paul Copplestone <pcopplestone@gmail.com>
2019-04-11 19:34:54 +0800

    Filtering out views from tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -4,5 +4,5 @@ select
   obj_description((table_schema || '."' || table_name || '"')::regclass, 'pg_class')
 
 from information_schema.tables
-where table_schema = ?
+where table_schema = ? and table_type = 'BASE TABLE'
 order by table_schema, table_name
